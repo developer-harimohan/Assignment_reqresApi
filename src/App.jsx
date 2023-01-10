60828b7c30db311df60c8032cd4453e9b25b5e43
developer-harimohan <harimohan.dev.05@gmail.com>
2023-01-10 17:31:13 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Common from './pages/Common.jsx'
 import GetExp from './pages/GetExp/GetExp'
 import Home from './pages/Home/Home'
 import PostExp from './pages/PostExp/PostExp'
